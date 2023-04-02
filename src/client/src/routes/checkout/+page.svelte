18da9fb91753e9121c7ddfe72e852626e7b3b9bb
DS <mailforgautamvg@gmail.com>
2023-04-02 20:53:48 +0530

    page now shows no receipts

--- src/client/src/routes/checkout/+page.svelte
+++ src/client/src/routes/checkout/+page.svelte
@@ -4,6 +4,7 @@
     import { fade } from "svelte/transition";
     import { Shadow } from "svelte-loading-spinners";
 
+    let ready = false;
     let receipts: Array<ParkingReceipt> = [];
 
     onMount(() => {
@@ -14,6 +15,7 @@
     async function update() {
         const res = await fetch("/api/receipts/list_payable");
         receipts = await res.json();
+        ready = true;
     }
 
     function handleClick(id: number) {
@@ -30,7 +32,7 @@
 
 </script>
 
-{#if receipts.length == 0}
+{#if !ready}
 
     <!-- Loader -->
     <div class="w-screen h-screen flex justify-center items-center">
@@ -42,35 +44,38 @@
     <!-- Outer Container -->
     <div class="flex flex-col gap-8 items-center mx-auto my-16" transition:fade>
         <h1 class="text-3xl">Pay up</h1>
-
-        <table class="table w-3/4">
-            <thead>
-                <tr>
-                    <th> Parking Spot </th>
-                    <th> Start time </th>
-                    <th> End time </th>
-                    <th> Time parked </th>
-                    <th> Parking fees </th>
-                    <th> Amount </th>
-                    <th> </th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each receipts as receipt}
-                    <tr transition:fade>
-                        <td>{receipt.parking_spot_name}</td>
-                        <td>{receipt.start_time}</td>
-                        <td>{receipt.end_time}</td>
-                        <td>{receipt.time_parked}</td>
-                        <td>{receipt.parking_fee}</td>
-                        <td>{receipt.amount}</td>
-                        <td>
-                            <button on:click={handleClick(receipt.id)} class="btn btn-primary">Pay</button>
-                        </td>
+        {#if receipts.length == 0}
+            <p class="text-lg">No receipts yet</p>
+        {:else}
+            <table class="table w-3/4" in:fade>
+                <thead>
+                    <tr>
+                        <th> Parking Spot </th>
+                        <th> Start time </th>
+                        <th> End time </th>
+                        <th> Time parked </th>
+                        <th> Parking fees </th>
+                        <th> Amount </th>
+                        <th> </th>
                     </tr>
-                {/each}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {#each receipts as receipt}
+                        <tr transition:fade>
+                            <td>{receipt.parking_spot_name}</td>
+                            <td>{receipt.start_time}</td>
+                            <td>{receipt.end_time}</td>
+                            <td>{receipt.time_parked}</td>
+                            <td>{receipt.parking_fee}</td>
+                            <td>{receipt.amount}</td>
+                            <td>
+                                <button on:click={handleClick(receipt.id)} class="btn btn-primary">Pay</button>
+                            </td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        {/if}
     </div>
 
 {/if}
