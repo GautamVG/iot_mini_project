ffe881e30d854a26e83eacc79fc5f2485c48b400
DS <mailforgautamvg@gmail.com>
2023-04-03 13:01:30 +0530

    added dist

--- dist/app.py
+++ dist/app.py
@@ -1,9 +1,11 @@
 from threading import Thread;
+from time import sleep;
 from math import floor, ceil;
 from datetime import datetime;
 from flask import Flask, abort, request, send_from_directory;
 from lib import db;
-from lib import parking_detector;
+from lib import parking_detector_sim as parking_detector;
+# from lib import parking_detector;
 
 # The flask application
 app = Flask(__name__);
@@ -69,6 +71,7 @@ def home(path):
 
 def event_loop():
     while True:
+        sleep(0.01);
         checked_in_spots, checked_out_spots = parking_detector.detect_parking();
 
         # For each checked in spot, insert a ticket into the database
--- dist/lib/parking_detector.py
+++ dist/lib/parking_detector.py
@@ -1,25 +1,57 @@
-from random import randint
+import RPi.GPIO as GPIO
+import time;
 from lib import db;
 
-# TODO: Interface with Sensors here
-# Currently this function generates data randomly
+dist_threshold = 5 # cm
+_echo_sensors = [3, 5, 7] # GPIO.BOARD 
+_trig_sensors = [11, 13, 15] # GPIO.BOARD 
+
+GPIO.setmode(GPIO.BOARD);
+for sensor in _echo_sensors:
+    GPIO.setup(sensor, GPIO.IN);
+for sensor in _echo_sensors:
+    GPIO.setup(sensor ,GPIO.OUT);
+
+def car_present_at_sensor(i):
+    # set Trigger to HIGH
+    GPIO.output(_trig_sensors[i], True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001);
+    GPIO.output(_trig_sensors[i], False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(_echo_sensors[i]) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(_echo_sensors[i]) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance < dist_threshold;
 
 def detect_parking():
     # Outputs 2 lists of Spot data
     # one list contains spots which just had a vehicle enter
     # other list contains spots which just had a vehicle leave
 
-    delay = 10000;
     spots = db.query("select * from `spots`;");
-    empty_spots = [spot for spot in spots if spot["occupied"] == 0];
-    occupied_spots = [spot for spot in spots if spot["occupied"] == 1];
     checked_in_spots = [];
     checked_out_spots = [];
 
-    if (len(empty_spots) > 0 and randint(0, delay) == 0):
-        checked_in_spots.append(empty_spots[randint(0, len(empty_spots) - 1)]);
-
-    if (len(occupied_spots) > 0 and randint(0, delay) == 0):
-        checked_out_spots.append(occupied_spots[randint(0, len(occupied_spots) - 1)]);
+    for i in range(0, len(spots)):
+        if not spots[i]["occupied"] and car_present_at_sensor(i):
+            checked_in_spots.append(spots[i]);
+        if spots[i]["occupied"] and not car_present_at_sensor(i):
+            checked_out_spots.append(spots[i]);
 
     return checked_in_spots, checked_out_spots;
